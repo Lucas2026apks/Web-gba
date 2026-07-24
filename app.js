ca30bff8ff466703a8ca577541b5aa29bed00e35
Lucas2026apks <kathiaespinoza01@gmail.com>
2026-07-24 07:45:18 -0400

    Fix syntax error in game list array

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ function cargarJuegosAutomaticos() {
   const misJuegos = [
     "Tekken Advance (Europe).gba",
     "Pokemon - Edicion Rojo Fuego (Spain).gba"
-    "geometri dash.gba"
+    "geometri dash.gba",
   ];
 
   const contenedorLista = document.getElementById("game-list");
